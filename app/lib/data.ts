43bcea0941e98ace2d67e1eb2fe3a02399ce8e51
waleed alahmad <abokhaled8222928@gmail.com>
2026-08-09 21:00:47 +0300

    convert fetch customer function to unCashed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import {unstable_noStore as noStore} from 'next/cache';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -168,6 +169,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
   try {
     const customers = await sql<CustomerField[]>`
       SELECT
