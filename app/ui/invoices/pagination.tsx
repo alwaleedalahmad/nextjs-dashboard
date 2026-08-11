f0680ff2296a82de8ec0aef31fde172b241d2349
waleed alahmad <abokhaled8222928@gmail.com>
2026-08-11 22:19:49 +0300

    fetch invoice data and mutiating data

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -3,18 +3,31 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { generatePagination } from '@/app/lib/utils';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 10
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  function createPageURL(page: number | string) {
+    const param = new URLSearchParams(searchParams);
+    if (typeof page === 'number') {
+      param.set('page', page.toString());
+    } else {
+      param.delete('page');
+    }
+    return `${pathname}?${param.toString()}`;
+  }
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 10 */}
 
-      {/* <div className="inline-flex">
+      {<div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -47,7 +60,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div> }
     </>
   );
 }
